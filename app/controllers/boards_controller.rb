4dfa73b781024f7bd229a65e32a3d0764989bf5a
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-11-28 01:26:56 -0700

    finish building out nav bar

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,25 +1,42 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :destroy, :update]
 
   def index
     @boards = Board.all
   end
 
   def show
+    @lists = @board.lists.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @board = Board.new(board_params)
+    if @board.update
+      redirect_to boards_path
+    else
+      render :edit
+    end
   end
 
-  def new
-  end
+  
 
-  def create
-  end
-
-  def delete
+  def destroy
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :boards
+
+  def get_initials
+    first_letter = self.first_name.split('').first
+    second_letter = self.last_name.split('').first
+    "#{first_letter.upcase}#{second_letter.upcase}"
+  end
 end
